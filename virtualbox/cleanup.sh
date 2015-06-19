10061a9f8ee0d708d3664b2a41e0aa5fd6f730bf
Ryan Meyer <ryan1234@gmail.com>
2015-06-19 10:37:51 -0700

    Update cleanup.sh

--- virtualbox/cleanup.sh
+++ virtualbox/cleanup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+rm -rf /usr/share/doc
+rm -rf /usr/share/man
+rm -rf /usr/src/virtualbox-guest*
+#rm -rf /var/cache
+rm -rf /var/lib/apt/lists
+rm -rf /usr/src/linux-headers*
+
 rm -rf /usr/share/locale/{af,am,ar,as,ast,az,bal,be,bg,bn,bn_IN,br,bs,byn,ca,cr,cs,csb,cy,da,de}
 rm -rf /usr/share/locale/{de_AT,dz,el,en_AU,en_CA,eo,es,et,et_EE,eu,fa,fi,fo,fr,fur,ga,gez,gl,gu}
 rm -rf /usr/share/locale/{haw,he,hi,hr,hu,hy,id,is,it,ja,ka,kk,km,kn,ko,kok,ku,ky,lg,lt,lv,mg,mi}
@@ -7,3 +14,5 @@ rm -rf /usr/share/locale/{mk,ml,mn,mr,ms,mt,nb,ne,nl,nn,no,nso,oc,or,pa,pl,ps,pt
 rm -rf /usr/share/locale/{rw,si,sk,sl,so,sq,sr,sr*latin,sv,sw,ta,te,th,ti,tig,tk,tl,tr,tt,ur,urd}
 rm -rf /usr/share/locale/{ve,vi,wa,wal,wo,xh,zh,zh_HK,zh_CN,zh_TW,zu}
 rm -rf /usr/share/locale/{crh, ia, nds, ug, uk}
+
+aptitude -y purge libgl1-mesa-dri
